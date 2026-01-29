0a5e993a4da21f26b6c56712e8a7a6da0ce96e35
Mohamed Nidhal Mrad <Nidhal.MRAD@radiofrance.com>
2026-01-29 23:48:53 +0100

    Add DailymotionPlayerSDKCore product (no Chromecast) to fix embed conflict

--- Package.swift
+++ Package.swift
@@ -10,12 +10,24 @@ let package = Package(
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "DailymotionPlayerSDK",
-      targets: ["DailymotionPlayerSDK",
-                "DailymotionChromecast",
-                "DailymotionGoogleCast",
-                "DailymotionAdvertisingServices" ,
-                "GoogleInteractiveMediaAds",
-                "OMSDK_Dailymotion"]),
+      targets: [
+        "DailymotionPlayerSDK",
+        "DailymotionChromecast",
+        "DailymotionGoogleCast",
+        "DailymotionAdvertisingServices",
+        "GoogleInteractiveMediaAds",
+        "OMSDK_Dailymotion"
+      ]
+    ),
+    .library(
+      name: "DailymotionPlayerSDKCore",
+      targets: [
+        "DailymotionPlayerSDK",
+        "DailymotionAdvertisingServices",
+        "GoogleInteractiveMediaAds",
+        "OMSDK_Dailymotion"
+      ]
+    ),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
